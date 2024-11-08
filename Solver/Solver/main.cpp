30841808142eb2576b828355d104694c20c07e5f
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-11-08 19:24:15 +0200

    hide some building folders and added C++ formatter

--- Solver/Solver/main.cpp
+++ Solver/Solver/main.cpp
@@ -1,33 +1,44 @@
 #if !defined(__clang__) && defined(__GNUC__)
-#include <bits/stdc++.h>
+#    include <bits/stdc++.h>
 using namespace std;
 #endif
 
 #ifndef LOCAL
-class debug{public:debug(bool=false){}template<class c>debug&operator<<(c&){return *this;}debug&operator<<(ostream&(*f)(ostream&)){return *this;}};
-#define imie(...) ""
-#define tabs(a) ""
+class debug {
+public:
+    debug(bool = false) {
+    }
+    template <class c>
+    debug &operator<<(c &) {
+        return *this;
+    }
+    debug &operator<<(ostream &(*f)(ostream &)) {
+        return *this;
+    }
+};
+#    define imie(...) ""
+#    define tabs(a) ""
 #else
-#include "Helpers.h"
+#    include "Helpers.h"
 #endif
 
 using namespace std;
 void solve() {
     // solution is here
-
 }
 
-int main(int argc, const char * argv[]) {
-    ios_base::sync_with_stdio(false);
+int main(int argc, const char *argv[]) {
+    ios_base::sync_with_stdio(true);
 #ifdef LOCAL
     ifstream in("input.txt");
     std::cin.tie(0);
     std::cin.rdbuf(in.rdbuf());
 #endif
-    
+
     int T;
     cin >> T;
-    for(int t = 0;t < T;++t) {
+    cout << imie(T) << endl;
+    for (int t = 0; t < T; ++t) {
         solve();
     }
     return 0;
