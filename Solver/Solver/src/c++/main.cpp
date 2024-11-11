ec5e409d6f4018202ec351ba5719cfdb773017e0
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-11-11 18:40:36 +0200

    updated c++ template

--- Solver/Solver/src/c++/main.cpp
+++ Solver/Solver/src/c++/main.cpp
@@ -27,21 +27,37 @@ public:
 using namespace std;
 void solve() {
     // solution is here
-}
-
-int main(int argc, const char *argv[]) {
-    ios_base::sync_with_stdio(true);
-#ifdef LOCAL
-    ifstream in("../input/input.txt");
-    std::cin.tie(0);
-    std::cin.rdbuf(in.rdbuf());
-#endif
-
     int T;
     cin >> T;
     cout << imie(T) << endl;
     for (int t = 0; t < T; ++t) {
-        solve();
     }
+}
+
+// ------------ CONFIGURATION -------------------------
+
+int main(int argc, const char *argv[]) {
+    ios_base::sync_with_stdio(true);
+#ifdef LOCAL
+    ifstream inLocal;
+    auto config = [&](const std::string &filename) {
+        inLocal = ifstream(filename);
+        std::cin.tie(0);
+        std::cin.rdbuf(inLocal.rdbuf());
+    };
+#endif
+
+    auto testSamples = [&config](int i) {
+        cout << "Test Case #" << i << endl;
+        ostringstream str;
+        str << i;
+        const auto fileName = string("../input/input_") + str.str() + ".txt";
+        config(fileName);
+        solve();
+    };
+    // Test Cases
+    testSamples(1);
+    testSamples(2);
+
     return 0;
 }
