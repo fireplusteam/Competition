8342d9bc610fae8029ec1ec70d27517d5a748fc9
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-11-11 19:41:20 +0200

    configured C++ task to output to build folder only

--- Solver/Solver/src/c++/main.cpp
+++ Solver/Solver/src/c++/main.cpp
@@ -22,17 +22,23 @@ public:
     #include "Helpers.h"
 #endif
 
-#include "Ext_Templates.h"
+// #include "Ext_Templates.h"
 
 using namespace std;
-void solve() {
-    // solution is here
-    int T;
-    cin >> T;
-    cout << imie(T) << endl;
-    for (int t = 0; t < T; ++t) {
+
+class Solver {
+
+public:
+    void solve() {
+        // solution is here
+        int T;
+        cin >> T;
+        cout << imie(T) << endl;
+        for (int t = 0; t < T; ++t) {
+            // cout << t << endl;
+        }
     }
-}
+};
 
 // ------------ CONFIGURATION -------------------------
 
@@ -47,17 +53,17 @@ int main(int argc, const char *argv[]) {
     };
 #endif
 
-    auto testSamples = [&config](int i) {
-        cout << "Test Case #" << i << endl;
-        ostringstream str;
-        str << i;
-        const auto fileName = string("../input/input_") + str.str() + ".txt";
+    auto testSamples = [&config](const string &i) {
+        cout << "----------------------------------------------" << endl;
+        cout << "Test Case #" << i << ":" << endl;
+        const auto fileName = string("../input/input_") + i + ".txt";
         config(fileName);
-        solve();
+        Solver solver;
+        solver.solve();
     };
     // Test Cases
-    testSamples(1);
-    testSamples(2);
+    testSamples("1");
+    testSamples("2");
 
     return 0;
 }
