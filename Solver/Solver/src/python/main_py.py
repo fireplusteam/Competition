1da0d307aa5d45c0a6dc6f4e37bd523bcf952662
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-12-03 11:01:35 +0200

    download code

--- Solver/Solver/src/python/main_py.py
+++ Solver/Solver/src/python/main_py.py
@@ -1,3 +1,6 @@
+__YEAR__ = 2024
+__DAY__ = 4
+
 DEBUG = True
 # DEBUG = False 
 #-----------------------------------------------------
@@ -23,19 +26,16 @@ import sys
 import fileinput, re
 import re
 
-# Solution is here 
-
-
-
+# Solution is here
 
 class Solver:
-
     def __init__(self):
         pass
     
     def solve(self, testCase):
         lines  = sys.stdin.read()
-        res = 0 # here is method
+        res = CHANGE_HERE_WITH_METHOD(lines) # here is method
+        
         print(res)
 
 
@@ -53,7 +53,29 @@ if DEBUG:
 
     
 #  Test Cases
-testCase(1)
+# testCase(1)
 # testCase(2)
 
+# ------------------------------------ download input script
+# python3 src/python/helper/get_input.py --year=2024 --day=3 > src/input/input_puzzle.txt
+import subprocess
+import os
+def download_content():
+    cache_path = "src/python/helper/cache.txt"
+    mode = "w+" if not os.path.exists(cache_path) else "r"
+    with open(cache_path, mode) as file:
+        is_downloaded = file.read()
+    with open(cache_path, "w+") as file:
+        identity = f"Year:{__YEAR__},Day:{__DAY__}"
+        if is_downloaded != identity:
+            process = subprocess.run(["python3", f"src/python/helper/get_input.py", f"--year={__YEAR__}", f"--day={__DAY__}"], stdout=subprocess.PIPE)
+            with open("src/input/input_puzzle.txt", "wb+") as puzzle:
+                if not b"Please don't repeatedly request this endpoint" in process.stdout:
+                    puzzle.write(process.stdout)
+                    file.write(identity)
+                else:
+                    file.write("")
+                    puzzle.write(b"")
+
+download_content()
 testCase("puzzle")
