664cf86eec1b6e3fd8ef9140fc74e26955eaf4e2
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-12-06 10:39:27 +0200

    reduced main_py

--- Solver/Solver/src/python/main_py.py
+++ Solver/Solver/src/python/main_py.py
@@ -1,8 +1,5 @@
 __YEAR__ = 2024
 __DAY__ = 6
-
-DEBUG = True
-# DEBUG = False 
 #-----------------------------------------------------
 import math
 import os
@@ -34,67 +31,26 @@ class Solver:
     def __init__(self):
         pass
     
-    def solve(self, testCase):
-        lines  = sys.stdin.read().strip().split("\n")
+    def solve(self, testCase: int, input: str):
+        lines  = input.strip().split("\n")
         ans = 0
         # Solution is here
         
         return ans
 
 
-if DEBUG:
-    def redirect(i):
-        file_path = f"src/input/input_{i}.txt"
-        with open(file_path, "r") as file:
-            r = file.read()
-            if len(r) == 0:
-                return False
-        sys.stdin = open(file_path, 'r')
-        return True
-    
-    def testCase(i):
-        print("----------------------------------------------")
-        print(f"Test Case #{i}:")
-        if not redirect(i):
-            print(f"  #{i} Skipped")
-            return
-        solver = Solver()
-        import time
-        st = time.time()
-        ans = solver.solve(i)
-        print(f"  #{i} Answer = {ans}")
-        print(f"  #{i} Time = {time.time() - st: .5f}s")
+#  Test Cases -----------------------------------------------------------------------
 
-    
-#  Test Cases
+def testCase(i):
+    solver = Solver()
+    utils.testCase(i, lambda i, input: solver.solve(i, input))
+
 testCase(1)
 # testCase(2)
 # testCase(3)
 # testCase(4)
 # testCase(5)
 
-# ------------------------------------ download input script
-# python3 src/python/helper/get_input.py --year=2024 --day=3 > src/input/input_puzzle.txt
-import subprocess
-import os
-def download_content():
-    cache_path = "src/python/helper/cache.txt"
-    mode = "w+" if not os.path.exists(cache_path) else "r"
-    with open(cache_path, mode) as file:
-        is_downloaded = file.read()
-    with open(cache_path, "w+") as file:
-        identity = f"Year:{__YEAR__},Day:{__DAY__}"
-        if is_downloaded != identity:
-            process = subprocess.run(["python3", f"src/python/helper/get_input.py", f"--year={__YEAR__}", f"--day={__DAY__}"], stdout=subprocess.PIPE)
-            with open("src/input/input_puzzle.txt", "wb+") as puzzle:
-                if not b"Please don't repeatedly request this endpoint" in process.stdout:
-                    puzzle.write(process.stdout)
-                    file.write(identity)
-                else:
-                    file.write("")
-                    puzzle.write(b"")
-        else:
-            file.write(identity)
 
-download_content()
+utils.download_content(__YEAR__, __DAY__)
 testCase("puzzle")
--- Solver/Solver/src/python/utils.py
+++ Solver/Solver/src/python/utils.py
@@ -1,4 +1,6 @@
-
+import os
+import sys
+import time
 
 def FourD(rows, columns, z, w, def_val):
     """
@@ -22,3 +24,52 @@ def TwoD(rows, columns, def_val):
     array[rows][columns]
     """
     return [[def_val for _ in range(columns)] for _ in range(rows)]
+
+# Input Parser------------------------------------------------------------------------------------------------------
+
+def get_input(i):
+    file_path = f"src/input/input_{i}.txt"
+    if not os.path.exists(file_path):
+        return None
+    with open(file_path, "r") as file:
+        r = file.read()
+        if len(r) == 0:
+            return None
+    return r
+
+def testCase(i, solver):
+    print("----------------------------------------------")
+    print(f"Test Case #{i}:")
+
+    if not (input:=get_input(i)):
+        print(f"  #{i} Skipped")
+        return
+    import time
+    st = time.time()
+    ans = solver(i, input)
+    print(f"  #{i} Answer = {ans}")
+    print(f"  #{i} Time = {time.time() - st: .5f}s")
+
+
+# ------------------------------------ download input script
+# python3 src/python/helper/get_input.py --year=2024 --day=3 > src/input/input_puzzle.txt
+import subprocess
+import os
+def download_content(__YEAR__, __DAY__):
+    cache_path = "src/python/helper/cache.txt"
+    mode = "w+" if not os.path.exists(cache_path) else "r"
+    with open(cache_path, mode) as file:
+        is_downloaded = file.read()
+    with open(cache_path, "w+") as file:
+        identity = f"Year:{__YEAR__},Day:{__DAY__}"
+        if is_downloaded != identity:
+            process = subprocess.run(["python3", f"src/python/helper/get_input.py", f"--year={__YEAR__}", f"--day={__DAY__}"], stdout=subprocess.PIPE)
+            with open("src/input/input_puzzle.txt", "wb+") as puzzle:
+                if not b"Please don't repeatedly request this endpoint" in process.stdout:
+                    puzzle.write(process.stdout)
+                    file.write(identity)
+                else:
+                    file.write("")
+                    puzzle.write(b"")
+        else:
+            file.write(identity)
