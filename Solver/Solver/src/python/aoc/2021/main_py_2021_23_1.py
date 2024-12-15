5578a837b921fee3dc583b78491d622d6852fa92
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-12-15 10:51:50 +0200

    optimized 2021 23 1

--- Solver/Solver/src/python/aoc/2021/main_py_2021_23_1.py
+++ Solver/Solver/src/python/aoc/2021/main_py_2021_23_1.py
@@ -44,9 +44,11 @@ class Solver:
         field = [[x for x in y] for y in field]
         d = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
-        target = """...........
-##A#B#C#D
- #A#B#C#D"""
+        target = """#############
+#...........#
+###A#B#C#D###
+  #A#B#C#D#
+  #########"""
         target = [[x for x in y] for y in target.split("\n")]
         n = len(field)
 
@@ -60,7 +62,7 @@ class Solver:
                         to = ord(field[i][j]) - ord("A")
                         to *= 2
                         to += ord("0")
-                        to = chr(to + 2)
+                        to = chr(to + 3)
                         field[i][j] = to
             return field
         field = mapp(field)
@@ -75,115 +77,127 @@ class Solver:
             return -1
 
         cost_map = {
-            "2": 1,
-            "4": 10,
-            "6": 100,
-            "8": 1000
+            "3": 1,
+            "5": 10,
+            "7": 100,
+            "9": 1000
         }
-        target_hash = utils.to_deep_hash(target)
+
+        def to_hash(field):
+            r = ""
+            for j in range(1, len(field[0]) - 1):
+                r += field[1][j]
+            for i in range(2, len(field) - 1):
+                for j in range(3, 10, 2):
+                    r += field[i][j]
+            return r
+
+        target_hash = to_hash(target)
         best_ans = 1e100
 
-        seen = set()
+        seen = {}
 
-        def rec(field, half, cost, deep=0):
+        def rec(cost, deep=0):
             nonlocal best_ans
             moves = []
             should_continue = True
+            pre_move_cost = 0
             while should_continue:
                 should_continue = False
-                for i in range(n):
-                    m = len(field[i])
-                    for j in range(m):
-                        if field[i][j].isdigit() and half[i][j] == True:
-                            ni, nj = i, j
-                            delta_j = j_ind(field[i][j]) - j
-                            while delta_j != 0:
+                i = 1
+                m = len(field[i])
+                for j in range(m):
+                    if field[i][j].isdigit():
+                        ni, nj = i, j
+                        delta_j = j_ind(field[i][j]) - j
+                        while delta_j != 0:
+                            nj += 1 if delta_j > 0 else -1
+                            if field[ni][nj] != '.':
+                                break
+                            if nj == j_ind(field[i][j]):
                                 nj += 1 if delta_j > 0 else -1
-                                if not nj in range(len(field[ni])) or field[ni][nj] != '.':
-                                    break
-                                if nj == j_ind(field[i][j]):
-                                    nj += 1 if delta_j > 0 else -1
-                                    break
-                            if delta_j != 0:
-                                nj -= 1 if delta_j > 0 else -1
-                            if nj != j_ind(field[i][j]):
-                                continue
+                                break
+                        if delta_j != 0:
+                            nj -= 1 if delta_j > 0 else -1
+                        if nj != j_ind(field[i][j]):
+                            continue
 
-                            while True:
-                                ni += 1
-                                if ni >= len(field) or field[ni][nj] != '.':
-                                    break
-                            ni -= 1
-                            if (ni == 1 and field[ni + 1][nj] == field[i][j]) or (ni == 2):
-                                should_continue = True
-                                move_cost = cost_map[field[i][j]]
-                                field[i][j], field[ni][nj] = field[ni][nj], field[i][j]
-                                half[i][j] = False
-                                cost += abs(i - ni) * \
-                                    move_cost + abs(j - nj) * move_cost
-                                moves.append((i, j, ni, nj))
-                            field_hash = utils.to_deep_hash(field)
+                        while True:
+                            ni += 1
+                            if field[ni][nj] != '.':
+                                break
+                        ni -= 1
+                        if (ni == 2 and field[ni + 1][nj] == field[i][j]) or (ni == 3):
+                            should_continue = True
+                            move_cost = cost_map[field[i][j]]
+                            field[i][j], field[ni][nj] = field[ni][nj], field[i][j]
+                            pre_move_cost += abs(i - ni) * \
+                                move_cost + abs(j - nj) * move_cost
+                            moves.append((i, j, ni, nj))
             # utils.debugPrint(field)
             # print(deep, "------------------")
 
             def restore():
                 for i, j, ni, nj in moves:
                     field[i][j], field[ni][nj] = field[ni][nj], field[i][j]
-                    assert (half[ni][nj] == False)
-                    half[i][j] = True
 
-            field_hash = utils.to_deep_hash(field)
+            field_hash = to_hash(field)
+            cost += pre_move_cost
             if field_hash == target_hash:
                 if best_ans > cost:
                     best_ans = cost
                     print("Found", best_ans)
                 restore()
-                return
+                return pre_move_cost
             if field_hash in seen:
+                if best_ans > cost + seen[field_hash]:
+                    # utils.debugPrint(field)
+                    best_ans = cost + seen[field_hash]
+                    print("Found", best_ans)
                 restore()
-                return
+                return seen[field_hash] + pre_move_cost
             if best_ans <= cost:
                 restore()
-                return
-            seen.add(field_hash)
-
-            for i in range(n):
+                return 1e100
+            ret = 1e100
+            seen[field_hash] = ret
+            for i in range(2, n - 1):
                 m = len(field[i])
-                for j in range(m):
-                    if field[i][j].isdigit() and half[i][j] == False:
-                        if i == 1 and j_ind(field[i][j]) == j and field[i + 1][j] == field[i][j]:
+                for j in range(3, m, 2):
+                    if field[i][j].isdigit():
+                        if i == 2 and j_ind(field[i][j]) == j and field[i + 1][j] == field[i][j]:
                             continue
-                        if i == 2 and j_ind(field[i][j]) == j:
+                        if i == 3 and j_ind(field[i][j]) == j:
                             continue
                         ni, nj = i, j
                         while True:
                             ni -= 1
-                            if ni < 0 or field[ni][nj] != '.':
+                            if field[ni][nj] != '.':
                                 break
                         ni += 1
-                        if ni == i:
+                        if ni != 1:
                             continue
                         for dj in (-1, 1):
                             nj = j
                             while True:
                                 nj += dj
-                                if not nj in range(0, len(field[ni])) or field[ni][nj] != '.':
+                                if field[ni][nj] != '.':
                                     break
-                                move_cost = cost_map[field[i][j]]
-                                field[i][j], field[ni][nj] = field[ni][nj], field[i][j]
-                                half[ni][nj] = True
-                                rec(field, half, cost + abs(i - ni) *
-                                    move_cost + abs(j - nj) * move_cost, deep + 1)
-                                field[i][j], field[ni][nj] = field[ni][nj], field[i][j]
-                                assert (half[ni][nj])
-                                half[ni][nj] = False
+                                if nj != j:
+                                    move_cost = cost_map[field[i][j]]
+                                    field[i][j], field[ni][nj] = field[ni][nj], field[i][j]
+                                    cost_delta = abs(
+                                        i - ni) * move_cost + abs(j - nj) * move_cost
+                                    lr = rec(cost + cost_delta, deep + 1)
+                                    ret = min(lr + cost_delta, ret)
+                                    field[i][j], field[ni][nj] = field[ni][nj], field[i][j]
                         continue
 
             restore()
+            seen[field_hash] = ret
+            return ret + pre_move_cost
 
-            seen.remove(field_hash)
-        half = utils.TwoD(n, len(target[0]), False)
-        rec(field, half, 0)
+        rec(0)
 
         ans = best_ans
 
@@ -197,7 +211,7 @@ def testCase(i):
     utils.testCase(i, lambda i, input: solver.solve(i, input))
 
 
-# testCase(1)
+testCase(1)
 # testCase(2)
 # testCase(3)
 # testCase(4)
