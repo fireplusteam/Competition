f1f616f096d76c022faa57e705d01de988535220
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2025-02-22 22:51:39 +0200

    refactored to support easily to debug a python solution for codeforces

--- Solver/Solver/src/c++/templates/code_forces_main_py.py
+++ Solver/Solver/src/c++/templates/code_forces_main_py.py
@@ -1,5 +1,3 @@
-DEBUG = True
-# -----------------------------------------------------
 from cmath import sqrt
 import collections
 from queue import Queue
@@ -17,7 +15,7 @@ def _debugPrint(*values: object, sep=" ", end="\n", file=None, flush=False):
     pass
 
 
-if "DEBUG" in globals() and DEBUG:
+if len(sys.argv) > 1 and sys.argv[1] == "DEBUG":
     sys.stdin = open("src/input/input_1.txt", "r")
 else:
     debugPrint = _debugPrint
@@ -65,63 +63,39 @@ def case_solver():
         i = j
     ab_collections.sort()
     ba_collections.sort()
-    # print("AB_collections", ab_collections)
-    # print("BA_collections", ba_collections)
-    # print("Common_collections", common_collections)
+
+    def primary_reduce(val, score):
+        if val - score >= 0:
+            return (val - score, 0)
+        else:
+            return (0, score - val)
+
+    def secondary_reduce(val, score):
+        if score > 1:
+            if val - score + 1 >= 0:
+                return (val - score + 1, 1)
+            else:
+                return (0, score - val)
+        return (val, score)
+
     for ab_score, _, _, _ in ab_collections:
-        if ab - ab_score >= 0:
-            ab -= ab_score
-            ab_score = 0
-        elif ab > 0:
-            ab_score -= ab
-            ab = 0
-        if ab_score > 1:
-            if ba - ab_score + 1 >= 0:
-                ba -= ab_score - 1
-                ab_score = 1
-            elif ba > 0:
-                ab_score -= ba
-                ba = 0
+        ab, ab_score = primary_reduce(ab, ab_score)
+        ba, ab_score = secondary_reduce(ba, ab_score)
         a -= ab_score
         b -= ab_score
-        # print("Calculating AB", ab_score, a, b, ab, ba)
-    # print("After AB collection scored", a, b, ab, ba)
     for _, ba_score, _, _ in ba_collections:
-        if ba - ba_score >= 0:
-            ba -= ba_score
-            ba_score = 0
-        elif ba > 0:
-            ba_score -= ba
-            ba = 0
-        if ba_score > 1:
-            if ab - ba_score + 1 >= 0:
-                ab -= ba_score - 1
-                ba_score = 1
-            elif ab > 0:
-                ba_score -= ab
-                ab = 0
-        # print("Calculating BA", ba_score, a, b, ab, ba)
+        ba, ba_score = primary_reduce(ba, ba_score)
+        ab, ba_score = secondary_reduce(ab, ba_score)
         a -= ba_score
         b -= ba_score
     for score, _, a_score, b_score in common_collections:
         a -= a_score
         b -= b_score
-        if ab - score >= 0:
-            ab -= score
-            score = 0
-        elif ab > 0:
-            score -= ab
-            ab = 0
-        if ba - score >= 0:
-            ba -= score
-            score = 0
-        elif ba > 0:
-            score -= ba
-            ba = 0
+        ab, score = primary_reduce(ab, score)
+        ba, score = primary_reduce(ba, score)
         a -= score
         b -= score
 
-    debugPrint("Calculated", a, b, ab, ba)
     if a >= 0 and b >= 0:
         print("YES")
     else:
