2b156208cb73d4c9b3d48a1e77700c22e6b9b4e2
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2025-02-23 09:12:25 +0200

    improved codeforce template

--- Solver/Solver/src/c++/templates/code_forces_main_py.py
+++ Solver/Solver/src/c++/templates/code_forces_main_py.py
@@ -5,20 +5,11 @@ from collections import defaultdict, deque
 from collections import OrderedDict
 import sys
 import re
+import os
 
 
 def debugPrint(*values: object, sep=" ", end="\n", file=None, flush=False):
     print(*values, sep=sep, end=end, file=file, flush=flush)
-
-
-def _debugPrint(*values: object, sep=" ", end="\n", file=None, flush=False):
-    pass
-
-
-if len(sys.argv) > 1 and sys.argv[1] == "DEBUG":
-    sys.stdin = open("src/input/input_1.txt", "r")
-else:
-    debugPrint = _debugPrint
 
 
 # ------------ Test Case Solver, solution is here ----------------
@@ -103,6 +94,69 @@ def case_solver():
 
 
 # ----------------------------------------------------
+# region fastio
+from io import BytesIO, IOBase
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._file = file
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+def _debugPrint(*values: object, sep=" ", end="\n", file=None, flush=False):
+    pass
+
+
+if len(sys.argv) > 1 and sys.argv[1] == "DEBUG":
+    sys.stdin = open("src/input/input_1.txt", "r")
+else:
+    debugPrint = _debugPrint
+    sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+    input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+
 T = int(input())
 for t in range(T):
     debugPrint(f"Test Case #{t}:")
---
+++ Solver/Solver/src/c++/templates/template_readme.md
@@ -0,0 +1,6 @@
+Python SortedList Library using Fenwick tree
+
+https://github.com/cheran-senthil/PyRival/blob/master/pyrival/data_structures/SortedList.py
+
+algo like
+https://github.com/cp-algorithms/cp-algorithms/blob/main/src/string/suffix-automaton.md
