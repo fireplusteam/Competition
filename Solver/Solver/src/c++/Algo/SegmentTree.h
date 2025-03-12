197d3626f6fd5aa52821bc3d0780747c71aaea04
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2025-03-12 11:57:03 +0200

    improved segmenttree

--- Solver/Solver/src/c++/Algo/SegmentTree.h
+++ Solver/Solver/src/c++/Algo/SegmentTree.h
@@ -11,53 +11,43 @@
 //     if (a.first > b.first)
 //         return b;
 //     return a;
-// }, [&]() -> ValType { return {INT_MAX, 0}; });
+// });
 //
 // Example to show the sum on a segment
-// SegmentTree<long long> seg(a, [&](long long a, long long b) { return a + b; }, [&]() { return 0; });
+// SegmentTree<long long> seg(a, [&](long long a, long long b) { return a + b; });
 // min on a segment
-// SegmentTree<long long> seg(a, [&](long long a, long long b) { return min(a, b); }, [&]() { return INT_MAX; });
+// SegmentTree<long long> seg(a, [&](long long a, long long b) { return min(a, b); });
 template <class T>
 class SegmentTree {
     vector<T> tree;
-    function<T(const T &, const T &)> func;
-    function<T()> defaultValue;
+    function<T(const T &, const T &, const T &)> func; // (T root, T leftChild, T rightChild)
     int n;
 
 public:
-    SegmentTree(
-        const vector<T> &arr,
-        const function<T(const T &, const T &)> &_func,
-        const function<T()> &_defaultValue
-    )
+    SegmentTree(const vector<T> &arr, const function<T(const T &, const T &, const T &)> &_func)
         : tree(arr.size() * 4),
           func(_func),
-          defaultValue(_defaultValue),
           n((int)arr.size()) {
         function<T(int, int, int)> build = [&](int vert, int left, int right) {
-            if (left > right)
-                return defaultValue();
+            assert(left <= right);
             if (left == right) {
                 tree[vert] = arr[left];
                 return tree[vert];
             }
-            int mid    = (left + right) / 2;
-            tree[vert] = func(build(vert * 2 + 1, left, mid), build(vert * 2 + 2, mid + 1, right));
+            int mid = (left + right) / 2;
+            if (left > mid)
+                return tree[vert] = build(vert * 2 + 2, mid + 1, right);
+            if (mid + 1 > right)
+                return tree[vert] = build(vert * 2 + 1, left, mid);
+            tree[vert] = func(tree[vert], build(vert * 2 + 1, left, mid), build(vert * 2 + 2, mid + 1, right));
             return tree[vert];
         };
         build(0, 0, n - 1);
     }
 
-    // change value on i index to val
     void set(int i, T val) {
-        auto value = [&](int vert, int left, int right) {
-            if (left > right)
-                return defaultValue();
-            return tree[vert];
-        };
         function<T(int, int, int)> _set = [&](int vert, int left, int right) {
-            if (left > right)
-                return defaultValue();
+            assert(left <= right);
             if (left == right) {
                 assert(left == i);
                 tree[vert] = val;
@@ -65,26 +55,58 @@ public:
             }
             int mid = (left + right) / 2;
             if (i <= mid)
-                tree[vert] = func(_set(vert * 2 + 1, left, mid), value(vert * 2 + 2, mid + 1, right));
+                if (mid + 1 <= right)
+                    tree[vert] = func(tree[vert], _set(vert * 2 + 1, left, mid), tree[vert * 2 + 2]);
+                else
+                    tree[vert] = _set(vert * 2 + 1, left, mid);
+            else if (left <= mid)
+                tree[vert] = func(tree[vert], tree[vert * 2 + 1], _set(vert * 2 + 2, mid + 1, right));
             else
-                tree[vert] = func(_set(vert * 2 + 2, mid + 1, right), value(vert * 2 + 1, left, mid));
+                tree[vert] = _set(vert * 2 + 2, mid + 1, right);
             return tree[vert];
         };
         assert(0 <= i && i < n);
         _set(0, 0, n - 1);
     }
 
-    // get min on a segment [i, j)
+    // void update(int i, int j, const function<T(const T &)> &updateFunc) {
+    //     j                                  -= 1;
+    //     function<T(int, int, int)> _update  = [&](int vert, int left, int right) {
+    //         if (left > right)
+    //             return defaultValue();
+    //         if (max(i, left) > min(j, right))
+    //             return tree[vert];
+    //         int mid = (left + right) / 2;
+    //         if (i <= left && right <= j) {
+    //             if (left == right)
+    //                 return tree[vert] = updateFunc(tree[vert]);
+    //             return tree[vert] = func(
+    //                        updateFunc(tree[vert]),
+    //                        value(vert * 2 + 1, left, mid),
+    //                        value(vert * 2 + 2, mid + 1, right)
+    //                    );
+    //         }
+    //         tree[vert] = func(tree[vert], _update(vert * 2 + 1, left, mid), _update(vert * 2 + 2, mid + 1, right));
+    //         return tree[vert];
+    //     };
+    //     assert(0 <= i && i <= j && j < n);
+    //     _update(0, 0, n - 1);
+    // }
+
     T get(int i, int j) {
         j                               -= 1;
         function<T(int, int, int)> _get  = [&](int vert, int left, int right) {
             if (max(i, left) > min(j, right))
-                return defaultValue();
+                return tree[vert];
             if (i <= left && right <= j) {
                 return tree[vert];
             }
             int mid = (left + right) / 2;
-            return func(_get(vert * 2 + 1, left, mid), _get(vert * 2 + 2, mid + 1, right));
+            if (max(left, i) > min(mid, i))
+                return _get(vert * 2 + 2, mid + 1, right);
+            if (max(mid + 1, i) > min(right, j))
+                return _get(vert * 2 + 1, left, mid);
+            return func(tree[vert], _get(vert * 2 + 1, left, mid), _get(vert * 2 + 2, mid + 1, right));
         };
         assert(0 <= i && i <= j && j < n);
         return _get(0, 0, n - 1);
