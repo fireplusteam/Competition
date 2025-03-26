fafe8f0df2d6fbf5eaa008061f3ec3fc10c781ac
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2025-03-26 19:11:50 +0200

    improved spare table added queue with ability to calculate and recalculate the value on all elements of queue in O(1)

--- Solver/Solver/src/c++/Algo/DisjoinedSpareTable.h
+++ Solver/Solver/src/c++/Algo/DisjoinedSpareTable.h
@@ -19,22 +19,31 @@ using namespace std;
 // Prebuild in O(nlog(n))
 // answer query in O(1)
 // set define function like [&](int a, int b) { return min(a, b); } or similar
+// Example of usage of calculating GCD on a range
+// vector<long long> a(n); // init state
+// DisjoinedSpareTable<long long> table(a.size(), [&](int index) {
+//     if (index < a.size()) {
+//         return a[index];
+//     }
+//     return 1LL;
+// }, [&](long long a, long long b) { return gcd(a, b); });
+template <class T>
 class DisjoinedSpareTable {
     int maxLev;
-    vector<int> table[32];
-    vector<int> A;
-    std::function<int(int, int)> func;
+    vector<T> table[32];
+    std::function<T(int)> initialValue; // init value at index
+    std::function<T(T, T)> func;
     void build(int level, int l, int r) {
         int m           = (l + r) / 2;
 
-        table[level][m] = A[m];
+        table[level][m] = initialValue(m);
         for (int i = m - 1; i >= l; i--)
-            table[level][i] = func(table[level][i + 1], A[i]);
+            table[level][i] = func(table[level][i + 1], initialValue(i));
 
         if (m + 1 < r) {
-            table[level][m + 1] = A[m + 1];
+            table[level][m + 1] = initialValue(m + 1);
             for (int i = m + 2; i < r; i++)
-                table[level][i] = func(table[level][i - 1], A[i]);
+                table[level][i] = func(table[level][i - 1], initialValue(i));
         }
         if (r - l > 1) // r - l > 1
         {
@@ -44,11 +53,11 @@ class DisjoinedSpareTable {
     }
 
 public:
-    DisjoinedSpareTable(const vector<int> &A, const std::function<int(int, int)> &func) {
-        this->A    = A;
-        this->func = func;
-        int size   = (int)A.size();
-        maxLev     = __builtin_clz(size) ^ 31;
+    DisjoinedSpareTable(int n, const std::function<T(int)> &_initValue, const std::function<T(T, T)> &func) {
+        this->initialValue = _initValue;
+        this->func         = func;
+        int size           = n;
+        maxLev             = __builtin_clz(size) ^ 31;
 
         if ((1 << maxLev) != size) {
             size = 1 << ++maxLev;
@@ -59,15 +68,57 @@ public:
         build(0, 0, size);
     }
 
-    int query(int x, int y) {
+    T query(int x, int y) {
         if (x == y)
-            return A[x];
+            return initialValue(x);
         int k2  = __builtin_clz(x ^ y) ^ 31;
         int lev = maxLev - 1 - k2;
         int ans = table[lev][x];
         if (y & ((1 << k2) - 1)) // y % (1<<k2)
             ans = func(ans, table[lev][y]);
         return ans;
+    }
+};
+
+
+// Simple SpareTable, not suitable for sum on a range, only for min/max/gcd
+template <class T>
+class SpareTable {
+    vector<int> lg;
+    vector<T> st[30];
+    int n;
+    function<T(T, T)> mergeFunc;
+
+public:
+    SpareTable(const vector<T> &arr, const function<T(T, T)> &_func)
+        : mergeFunc(_func) {
+        n = (int)arr.size();
+        lg.resize(n + 1);
+        lg[1] = 0;
+        for (int i = 2; i <= n; ++i) {
+            lg[i] = lg[i / 2] + 1;
+        }
+        st[0]      = arr;
+        auto power = [&](int p) {
+            int r = 0;
+            while (p > 0) {
+                r  += 1;
+                p >>= 1;
+            }
+            return r + 1;
+        };
+        const int K = power(n);
+        for (int i = 1; i <= K; ++i) {
+            st[i].resize(n);
+            for (int j = 0; j + (1 << i) <= n; ++j) {
+                st[i][j] = mergeFunc(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+            }
+        }
+    }
+
+    T query(int l, int r) {
+        int i = lg[r - l + 1];
+        return mergeFunc(st[i][l], st[i][r - (1 << i) + 1]);
     }
 };
 
---
+++ Solver/Solver/src/c++/Algo/Queue.h
@@ -0,0 +1,114 @@
+#ifndef queue_header_h
+#define queue_header_h
+
+#include <stdio.h>
+#include <algorithm>
+#include <cassert>
+#include <complex>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+
+using namespace std;
+
+// Used if we need to perform some kind of operation on the whole elements of queue like gcd(a0, a1, a2, a3, ...) in
+// O(1) time
+template <class Value, class State>
+class Queue {
+    vector<pair<Value, State>> stack;
+    vector<pair<Value, State>> reversed;
+    function<State(const State &, Value)> pushFunc;
+    int _size = 0;
+
+public:
+    Queue(const function<State(const State &, Value)> &_pushFunc)
+        : pushFunc(_pushFunc) {
+    }
+    void push(Value val) {
+        _size++;
+        stack.push_back({val, pushFunc(stack.empty() ? State() : stack.back().second, val)});
+    }
+    void pop() {
+        assert(_size > 0);
+        --_size;
+        if (reversed.empty()) {
+            reversed.resize(stack.size() - 1);
+            if (reversed.size() > 0) {
+                reversed[0] = {stack.back().first, pushFunc(State(), stack.back().first)};
+                for (int i = 1; i < reversed.size(); ++i)
+                    reversed[i] = {
+                        stack[(int)stack.size() - i - 1].first,
+                        pushFunc(reversed[i - 1].second, stack[(int)stack.size() - i - 1].first)
+                    };
+            }
+            stack.clear();
+            return;
+        }
+        reversed.pop_back();
+    }
+    template <class RetValue>
+    RetValue value(const function<RetValue(const State &, const State &)> mergeFunc) {
+        return mergeFunc(
+            reversed.empty() ? State() : reversed.back().second,
+            stack.empty() ? State() : stack.back().second
+        );
+    }
+    void clear() {
+        stack.clear();
+        reversed.clear();
+    }
+    int size() const {
+        return this->_size;
+    }
+};
+
+/// example of Usage on:
+/// Given an array of 𝑛 integers. Let's say that a segment of this array 𝑎[𝑙..𝑟] (1≤𝑙≤𝑟≤𝑛)is good if the GCD of all
+/// numbers on this segment is
+//     1 .Your task is to find the shortest good segment.
+//     Input:
+// The first line contains an integer 𝑛(1≤𝑛≤105).The second line contains integers 𝑎𝑖(1≤𝑎𝑖≤1018).class State {
+
+long long getShortestSegmentWithGCDOne(const vector<long long> &a) {
+    class State {
+    public:
+        long long val;
+        State(long long _val = 0LL)
+            : val(_val) {
+        }
+    };
+    function<long long(long long, long long)> gcd = [&](long long a, long long b) {
+        if (a == 0 || b == 0)
+            return max(a, b);
+        while (b > 0) {
+            auto c = b;
+            b      = a % b;
+            a      = c;
+        }
+        return a;
+    };
+    int n = (int)a.size();
+    Queue<long long, State> q([&](const State &state, long long b) { return State(gcd(state.val, b)); });
+    auto merge    = [&](const State &a, const State &b) { return gcd(a.val, b.val); };
+    int j         = 0;
+    long long ans = -1;
+    for (int i = 0; i < n; ++i) {
+        j = max(j, i);
+        while (j < n && (q.size() == 0 || q.value<long long>(merge) > 1)) {
+            q.push(a[j]);
+            ++j;
+        }
+        if (q.value<long long>(merge) == 1) {
+            if (ans == -1 || ans > j - i)
+                ans = j - i;
+        } else
+            break;
+        q.pop();
+    }
+    return ans;
+}
+#endif
