29857852b03f61b94bace18d28a5feffc82dd11c
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2025-04-27 18:41:17 +0300

    updated c++ template

--- Solver/Solver/src/c++/templates/code_forces_main.cpp
+++ Solver/Solver/src/c++/templates/code_forces_main.cpp
@@ -9,10 +9,10 @@ public:
     debug(bool = false) {
     }
     template <class c>
-    debug &operator<<(c &) {
+    debug& operator<<(c&) {
         return *this;
     }
-    debug &operator<<(ostream &(*f)(ostream &)) {
+    debug& operator<<(ostream& (*f)(ostream&)) {
         return *this;
     }
 };
@@ -22,25 +22,38 @@ public:
     #include "Helpers.h"
 #endif
 
-using namespace std;
-void solve() {
-    // solution is here
+
+void precalculate() {
+    // data needed for all test cases
 }
 
-int main(int argc, const char *argv[]) {
+void solve() {
+}
+
+int main(int argc, const char* argv[]) {
     ios_base::sync_with_stdio(false);
 #ifdef LOCAL
     ifstream in("input_1.txt");
     // freopen("input_1.txt", "r", stdin);
     std::cin.tie(0);
     std::cin.rdbuf(in.rdbuf());
+
+    clock_t start = clock();
 #endif
+    precalculate();
 
     int T;
     cin >> T;
     for (int t = 0; t < T; ++t) {
-        debug() << "Test Case #" << t << "       ---------------------------------------" << endl;
+        debug() << "Test Case #" << t << "       ---------------------------------------";
         solve();
     }
+
+#ifdef LOCAL
+    clock_t end         = clock();
+    double elapsed_time = double(end - start) / CLOCKS_PER_SEC;
+    std::cout << "Execution time: " << elapsed_time << " seconds" << std::endl;
+#endif
+
     return 0;
 }
